0b15541dcd7129e61433f20551bba612467c7a33
Ana <ana.delprado@gmail.com>
2017-08-17 20:19:06 -0300

    detalhes do filme

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -20,6 +20,8 @@ import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.modelo.DetalhesDoFilme;
+import br.com.caelum.ingresso.rest.ImdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -34,6 +36,9 @@ public class FilmeController {
 
 	@Autowired
 	private SessaoDao sessaoDao;
+	
+	@Autowired
+	private ImdbClient client;
 	
     @GetMapping({"/admin/filme", "/admin/filme(value=/{id}"})
     public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme){
@@ -101,8 +106,11 @@ public class FilmeController {
         
         Filme filme = filmeDao.findOne(id);
         List <Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
+        
+        Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
 
         modelAndView.addObject("sessoes", sessoes);
+        modelAndView.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
 
         return modelAndView;
     }
